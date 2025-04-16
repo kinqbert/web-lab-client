14d77b801df5d11c6a09c86208ec45da65204a1a
kinqbert <perehodko1010@gmail.com>
2025-04-16 19:43:14 +0300

    header implemented
    Co-authored-by: kkkerber <valeriiakerber@gmail.com>

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import logo from "@/assets/general/logo.svg";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
+
+import styles from "./layout.module.css";
+
+export default function MailLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.content}>
+      <nav className={styles.navbar}>
+        <Link href="/dashboard">
+          <Image className={styles.logo} src={logo} alt="Logo" />
+        </Link>
+        <div className={styles.links}>
+          <Link
+            href="/dashboard"
+            className={`${styles.link} ${
+              pathname === "/dashboard" ? styles.active : ""
+            }`}
+          >
+            Dashboard
+          </Link>
+
+          <Link
+            href="/transactions"
+            className={`${styles.link} ${
+              pathname === "/transactions" ? styles.active : ""
+            }`}
+          >
+            Transactions
+          </Link>
+        </div>
+      </nav>
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+}
---
+++ src/app/(main)/transactions/page.tsx
@@ -0,0 +1,3 @@
+export default function TransactionsBage() {
+  return <div>Transactions</div>;
+}
