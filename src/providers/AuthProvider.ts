2942d7825abfcb6c11abcf20b9d03d71dda7c907
kinqbert <perehodko1010@gmail.com>
2025-04-16 18:16:19 +0300

    minor jwt fixes

--- src/providers/AuthProvider.ts
+++ src/providers/AuthProvider.ts
@@ -2,6 +2,7 @@
 import { useEffect } from "react";
 import { useAuthStore } from "@/store/authStore";
 import { api } from "@/api/api";
+import { redirect } from "next/navigation";
 
 export default function AuthProvider({
   children,
@@ -16,7 +17,8 @@ export default function AuthProvider({
         const { data } = await api.post("/auth/refresh");
         setAccessToken(data.accessToken);
       } catch {
-        // setAccessToken(null);
+        setAccessToken(null);
+        redirect("/login");
       }
     })();
   }, [setAccessToken]);
